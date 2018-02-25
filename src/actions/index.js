2f950c6e7313821dd4964c66eb404c102e616bf0
Tyler Speaks <speaks_tyler@yahoo.com>
2018-02-24 21:26:40 -0600

    netlify

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,26 +2,26 @@ import axios from "axios";
 import { FETCH_USER, FETCH_OFFERS } from "./types";
 
 export const fetchUser = () => async dispatch => {
-	const res = await axios.get("/api/current_user");
+	const res = await axios.get("https://next-offer-server.herokuapp.com/api/current_user");
 
 	dispatch({ type: FETCH_USER, payload: res.data });
 };
 
 export const handleToken = token => async dispatch => {
-	const res = await axios.post("/api/stripe", token);
+	const res = await axios.post("https://next-offer-server.herokuapp.com/api/stripe", token);
 
 	dispatch({ type: FETCH_USER, payload: res.data });
 };
 
 export const submitOffer = (values, history) => async dispatch => {
-	const res = await axios.post("/api/offers", values);
+	const res = await axios.post("https://next-offer-server.herokuapp.com/api/offers", values);
 
 	history.push("/offers");
 	dispatch({ type: FETCH_USER, payload: res.data });
 };
 
 export const fetchOffers = () => async dispatch => {
-	const res = await axios.get("/api/offers");
+	const res = await axios.get("https://next-offer-server.herokuapp.com/api/offers");
 
 	dispatch({ type: FETCH_OFFERS, payload: res.data });
 };
